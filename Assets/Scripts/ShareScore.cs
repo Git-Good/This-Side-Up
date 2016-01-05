89ed31c23e29e5c496c219621ae9b5c79fef343e
krytical <johnnie-l@Live.ca>
2016-01-04 20:36:24 -0800

    Reverted back code
    - Previous change broke leaderboard

--- Assets/Scripts/ShareScore.cs
+++ Assets/Scripts/ShareScore.cs
@@ -10,13 +10,11 @@ public class ShareScore : MonoBehaviour {
 	public void SendScore() {
 		HScore = PlayerPrefs.GetInt ("HighScore");
 		// Don't share score if 0
-		if (HScore != 0){
 			if (PlayGamesPlatform.Instance.localUser.authenticated) {
 				PlayGamesPlatform.Instance.ReportScore (HScore, "CgkIsJ6QpO0HEAIQAA", (bool success) => {
 					PlayGamesPlatform.Instance.ShowLeaderboardUI ("CgkIsJ6QpO0HEAIQAA");
 				});
-			}
-		} else {
+			} else {
 			PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder ().Build ();
 			GooglePlayGames.PlayGamesPlatform.InitializeInstance (config);
 			//GooglePlayGames.PlayGamesPlatform.DebugLogEnabled = true;
