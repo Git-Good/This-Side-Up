b5ee0aa991ccf74c1d7b48b4aa47c7ad25da4123
lolpolice <tianyanggu@hotmail.com>
2016-01-19 01:21:10 -0800

    Fixed Best Score Colour Not Changing Bug
    oldscore variable used to fix it

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -6,6 +6,7 @@ public class Score : MonoBehaviour {
 
 	private static int score { get; set; }
 	static int highScore;
+	static int oldScore;
 	Text scoreText, highScoreText;
 	bool newScore = false;
 
@@ -21,30 +22,36 @@ public class Score : MonoBehaviour {
 		newScore = false;
 		score = 0;
 		highScore = PlayerPrefs.GetInt ("HighScore");
+
 		//Debug.Log("High Score: " + PlayerPrefs.GetInt ("HighScore"));
 		scoreText = gameObject.GetComponent<Text> ();
-		//highScoreText = gameObject.GetComponent<Text> ();
+
 	}
 
 	public void AddPoint(){
 		scoreS.PlayOneShot (scoreSound);
 		score++;
 		scoreText.text = "" + score;
+		oldScore = score;
 		if (score > highScore) {
 			newScore = true;
 			scoreText.color = new Color32 (238, 232, 170, 255);
+			oldScore = score + 1;
 			highScore = score;
 			PlayerPrefs.SetInt("HighScore", highScore);
 		}
 	}
 
 	public void ShowHighScore () {
-		if (newScore == true) {
-			Debug.Log ("Changed HS Color");
-			highScoreText.color = new Color32 (238, 232, 170, 255);
-		}
 		highScoreText = gameObject.GetComponent<Text> ();
 		highScoreText.text = "best: " + highScore;
+
+		if (oldScore >= highScore) {
+			Debug.Log ("Changed HS Color");
+			highScoreText.color = new Color32 (238, 232, 170, 255);
+			//highScoreText.color = new Color32 (0, 0, 0, 0);
+			oldScore = 0;
+		}
 	}
 
 	public void OnDestroy(){
